fa84846d782ed8718e963b2fcd767f1a69a7fab8
Ernest C. Davis <Ernest C. Davis>
2017-05-19 23:16:28 -0400

    Final Submission

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,21 +1,21 @@
 
 var friends = [
 	{
-		name: 'Donald Trump',
-		photo: 'http://theresurgent.com/wp-content/uploads/2016/02/nbc-fires-donald-trump-after-he-calls-mexicans-rapists-and-drug-runners-1.jpg',
-		scores: [1, 5, 1, 1, 5, 5, 5, 1, 5, 5]
+		name: 'Dave Chappelle',
+		photo: 'https://vignette1.wikia.nocookie.net/uncyclopedia/images/3/36/Dave_chappelle.jpg/revision/latest?cb=20110603055727',
+		scores: [1, 5, 2, 1, 4, 5, 1, 2, 5, 4]
 	},{
-		name: 'Alex Morgan',
-		photo: 'http://www.famousbirthdays.com/headshots/alex-morgan-3.jpg',
-		scores: [4, 1, 4, 5, 1, 1, 1, 5, 1, 1]
+		name: 'Martin Lawrence',
+		photo: 'http://s3.amazonaws.com/root-slideshow-prod/images%2Fslides%2Fmartin_lawrence_then_082812_400jrw.jpg',
+		scores: [4, 1, 4, 5, 1, 2, 1, 4, 1, 1]
 	},{
-		name: 'Phil Dunphy',
-		photo: 'https://cdn.pastemagazine.com/www/blogs/lists/2012/07/20/phil-dunphy.jpg',
-		scores: [3, 3, 3, 3, 3, 3, 3, 3, 3, 3]
+		name: 'Chris Rock',
+		photo: 'https://s3.amazonaws.com/webassets.ticketmob.com/LS/images/comedians/Chris-Rock.jpg',
+		scores: [3, 2, 3, 4, 3, 1, 3, 3, 4, 2]
 	},{
-		name: 'Natalie Coughlin',
-		photo: 'http://img.mindbodygreen.com/image/upload/c_limit,w_738,f_auto/ftr/natalie-olympics.jpg',
-		scores: [5, 1, 5, 2, 1, 1, 1, 5, 1, 1]
+		name: 'Eddie Murphy',
+		photo: 'http://anygoodfilms.com/wp-content/uploads/2017/04/eddie-murphy.jpg',
+		scores: [5, 2, 5, 2, 5, 3, 5, 5, 1, 3]
 	}];
 
 module.exports = friends;
